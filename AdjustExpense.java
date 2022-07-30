6a5f81f847738d1b2721f01e69748cd0f933ec00
Navneet Das <navneetdas53@gmail.com>
2022-07-30 23:04:23 +0530

    Update AdjustExpense.java

--- AdjustExpense.java
+++ AdjustExpense.java
@@ -1,20 +1,17 @@
 import Expense;
 
 public class AdjustExpense {
-  Expense createExpense(int amount, int whoPaid, List<Split> parts, Type type) {
+  public Expense createExpense(int amount, int whoPaid, List<Split> parts, Type type) {
     if (type == PERC) {
-      for (int i = 0; i < parts.size(); i++) {
-        parts[i].setAmount((amount * parts[i].getPerc()) / 100);
-      }
+      for (int i = 0; i < parts.size(); i++) parts[i].setAmount((amount * parts[i].getPerc()) / 100);
     } else if (type == EQ) {
       int eqAmount = amount / parts.size();
-      for (int i = 0; i < parts.size(); i++) {
-        parts[i].setAmount(eqAmount);
-      }
+      for (int i = 0; i < parts.size(); i++) parts[i].setAmount(eqAmount);
     }
     return new Expense(amount, whoPaid, parts, type);
   }
 }
 
+// Data structure finalised to be used
 // unordered_map<int, unordered_map<int, int>>
 // unordered_map<id, unordered_map<_id, amount>>
