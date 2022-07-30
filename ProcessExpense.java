8ee71963b3a5303f28b66aff3cbeb1f076f1f322
Navneet Das <navneetdas53@gmail.com>
2022-07-30 23:07:21 +0530

    Update ProcessExpense.java

--- ProcessExpense.java
+++ ProcessExpense.java
@@ -1,28 +1,32 @@
 import AdjustExpense;
 
 public class ProcessExpense {
-  List<Expense> expenses;
-  Map<int, int> balances;
-  Map<int, Map<int, int>> ledger;
+  private final List<Expense> expenses;
+  private final Map<int, int> balances;
+  private final Map<int, Map<int, int>> ledger; // Makes sure debts are simplified
   
-  processExpense(int amount, int whoPaid, List<Split> parts, Type type) {
+  public void processExpense(int amount, int whoPaid, List<Split> parts, Type type) {
     Expense expense = createExpense(amount, whoPaid, parts, type);
     expenses.add(expense);
+    
     for (int i = 0; i < expense.getParts().size(); i++) {
       int id = parts[i].getPerson().getId();
+      
       int newBal = balances[id] + parts[i].getAmount();
       balances[id] = newBal;
       int newBalAlt = balances[whoPaid] - parts[i].getAmount();
       balances[whoPaid] = newBalAlt;
+      
       ledger[id][whoPaid] = ledger[id][whoPaid] + newBalAlt;
+      ledger[whoPaid][id] = ledger[whoPaid][id] - newBalAlt;
     }
   }
   
-  showBalace(int id) {
+  public void showBalance(int id) {
     System.out.println(balances[id]);
   }
   
-  showBalances() {
-    
+  public void showAllBalances() {
+    // Implement
   }
 }
